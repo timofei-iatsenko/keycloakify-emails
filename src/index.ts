827bbe26bb1a0d3846814cc329537a2fdda3a822
Timofei Iatsenko <timiatsenko@gmail.com>
2024-12-05 14:30:00 +0100

    fix: export `createVariablesHelper` correctly

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./build-theme/build-email-theme.js";
 export type { BuildEmailThemeOptions } from "./build-theme/types.js";
 export type * from "./types.js";
 export type * from "./kc-email-vars.js";
+export { createVariablesHelper } from "./create-variables-helper.js";
